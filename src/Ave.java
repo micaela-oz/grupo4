aa4e815db29f20be39e410046b7fd35692b6c0ac
unknown <rlmedici.aceleracion2024@gylgroup.com>
2024-08-26 14:41:03 -0300

    Creacion de clase Ave

---
+++ src/Ave.java
@@ -0,0 +1,11 @@
+public class Ave extends SerVivo{
+
+    @Override
+    public void respirar(){
+        System.out.println("El ave esta respirando");
+    }
+
+    public void volar(){
+        System.out.println("El ave esta volando");
+    }
+}
