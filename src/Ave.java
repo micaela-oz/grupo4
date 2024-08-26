8806e002c38a76eb99e9bf7b7326200a7491406b
unknown <rlmedici.aceleracion2024@gylgroup.com>
2024-08-26 15:19:21 -0300

    Test

--- src/Ave.java
+++ src/Ave.java
@@ -1,5 +1,11 @@
 public class Ave extends SerVivo{
 
+    String name;
+
+    Ave(String name){
+        this.name = name;
+    }
+
     @Override
     public void respirar(){
         System.out.println("El ave esta respirando");
@@ -7,5 +13,12 @@ public class Ave extends SerVivo{
 
     public void volar(){
         System.out.println("El ave esta volando");
+    }
+
+    public static void main(String[] args){
+    Ave Cotorra = new Ave("Pepita");
+
+    Cotorra.volar();
+
     }
 }
